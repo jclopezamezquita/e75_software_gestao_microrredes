620dc02976c482acbdf8efe310682388d7de2ebd
Juan Camilo <jclopeza@unicamp.br>
2022-02-01 12:03:03 -0300

    genset dispatch added

--- angular/Frontend/src/app/modules/dispatch/dispatch.component.ts
+++ angular/Frontend/src/app/modules/dispatch/dispatch.component.ts
@@ -13,6 +13,7 @@ export class DispatchComponent implements OnInit {
   public dispatchData_BESS = new Array();
   public dispatchData_load = new Array();
   public dispatchData_PV = new Array();
+  public dispatchData_genset = new Array();
 
   constructor(private http: HttpClient) {
 
@@ -102,6 +103,33 @@ export class DispatchComponent implements OnInit {
           this.dispatchData_PV.push(Number(data2.pv_curt_t23));
           // console.log("this.dispatchData_PV: ");
           // console.log(this.dispatchData_PV);
+
+          this.dispatchData_genset = [];
+          this.dispatchData_genset.push(Number(data2.genset_power_t00));
+          this.dispatchData_genset.push(Number(data2.genset_power_t01));
+          this.dispatchData_genset.push(Number(data2.genset_power_t02));
+          this.dispatchData_genset.push(Number(data2.genset_power_t03));
+          this.dispatchData_genset.push(Number(data2.genset_power_t04));
+          this.dispatchData_genset.push(Number(data2.genset_power_t05));
+          this.dispatchData_genset.push(Number(data2.genset_power_t06));
+          this.dispatchData_genset.push(Number(data2.genset_power_t07));
+          this.dispatchData_genset.push(Number(data2.genset_power_t08));
+          this.dispatchData_genset.push(Number(data2.genset_power_t09));
+          this.dispatchData_genset.push(Number(data2.genset_power_t10));
+          this.dispatchData_genset.push(Number(data2.genset_power_t11));
+          this.dispatchData_genset.push(Number(data2.genset_power_t12));
+          this.dispatchData_genset.push(Number(data2.genset_power_t13));
+          this.dispatchData_genset.push(Number(data2.genset_power_t14));
+          this.dispatchData_genset.push(Number(data2.genset_power_t15));
+          this.dispatchData_genset.push(Number(data2.genset_power_t16));
+          this.dispatchData_genset.push(Number(data2.genset_power_t17));
+          this.dispatchData_genset.push(Number(data2.genset_power_t18));
+          this.dispatchData_genset.push(Number(data2.genset_power_t19));
+          this.dispatchData_genset.push(Number(data2.genset_power_t20));
+          this.dispatchData_genset.push(Number(data2.genset_power_t21));
+          this.dispatchData_genset.push(Number(data2.genset_power_t22));
+          this.dispatchData_genset.push(Number(data2.genset_power_t23));
+
     });
 
   }
